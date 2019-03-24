e24847fa659d2463026d35cd099617424e0f618b
Robert Grumbine <Robert.Grumbine@gmail.com>
2019-03-23 21:20:29 -0400

    iterate on the C program

--- eniac.proj/F77/ENIAC.f
+++ eniac.proj/F77/ENIAC.f
@@ -39,7 +39,6 @@ C      Declare data, physical info.
       
 C      Declare computational variables
       INTEGER i, j, k, l, m
-      REAL zi(0:p,0:q)
       
 C***********************************************************!!
 C      Set up the initial conditions deck
@@ -91,9 +90,11 @@ C      Rescale the heights to metric
       DO 1 j = 0, 15
         DO 2 i = 0, 18
           z(i,j)  = z(i,j)/fttom
-          zi(i,j) = z(i,j)
     2   CONTINUE
     1 CONTINUE
+CD      WRITE (*,9001) ((z(i,j),j=0,q),i=0,p)
+CD      WRITE (*,9002)
+CD      STOP
 
 C      Prepare the data decks - used due to memory constraints
       DO 10 i = 1, p-1
@@ -102,7 +103,7 @@ C      Prepare the data decks - used due to memory constraints
    11   CONTINUE
    10 CONTINUE
    
-         DO 20 j = 1, q-1
+      DO 20 j = 1, q-1
         DO 21 m = 1, q-1
           t(m,j) = SIN(pi*m*j/q)
    21   CONTINUE
@@ -115,7 +116,7 @@ C      Prepare the data decks - used due to memory constraints
    31   CONTINUE
    30 CONTINUE
    
-         DO 40 i = 0, p
+      DO 40 i = 0, p
         DO 41 j = 0, q
           r(i,j) = (ds/(2*radius))**2
      1           *((i-ip)**2+(j-jp)**2)
@@ -128,7 +129,7 @@ C      Prepare the data decks - used due to memory constraints
    51   CONTINUE
    50 CONTINUE
    
-         DO 60 i = 0, p
+      DO 60 i = 0, p
         DO 61 j = 0, q
           h(i,j) = g/(2*omega*ds)**2 
      1          * (1+r(i,j))**3/(1.-r(i,j))
@@ -157,6 +158,10 @@ C      Prepare the data decks - used due to memory constraints
    81   CONTINUE
    80 CONTINUE
    
+CD      WRITE (*,9001) ((z(i,j),j=0,q),i=0,p)
+CD      WRITE (*,9002)
+CD      STOP
+
 C     END OF THE PRELIMINARY SET UP SECTION
 C***********************************************************!!
 C      BEGIN THE ITERATIVE SOLUTION OF THE EQUATIONS
@@ -168,23 +173,27 @@ C      BEGIN THE ITERATIVE SOLUTION OF THE EQUATIONS
  1020   CONTINUE
  1010 CONTINUE
 
-        DO 1101 i = 1, p-1
-          IF (z(i+1,0) .LT. z(i-1,0)) THEN
-            eta(i,j) = f(i,j)+h(i,j)*zeta(i,j)
-          ENDIF
-           IF (z(i+1,q) .GT. z(i-1,q)) THEN
-            eta(i,j) = f(i,j)+h(i,j)*zeta(i,j)
-          ENDIF
- 1101   CONTINUE
+      DO 1101 i = 1, p-1
+        j = 0
+        IF (z(i+1,0) .LT. z(i-1,0)) THEN
+          eta(i,j) = f(i,j)+h(i,j)*zeta(i,j)
+        ENDIF
+        j = q
+        IF (z(i+1,q) .GT. z(i-1,q)) THEN
+          eta(i,j) = f(i,j)+h(i,j)*zeta(i,j)
+        ENDIF
+ 1101 CONTINUE
 
-        DO 1111 j = 1, q-1
-          IF (z(0,j+1).GT.z(0,j-1)) THEN
-            eta(i,j) = f(i,j)+h(i,j)*zeta(i,j)
-          ENDIF
-          IF (z(p,j+1).LT.z(p,j-1)) THEN 
-            eta(i,j) = f(i,j)+h(i,j)*zeta(i,j)
-          ENDIF
- 1111   CONTINUE
+      DO 1111 j = 1, q-1
+        i = 0
+        IF (z(0,j+1).GT.z(0,j-1)) THEN
+          eta(i,j) = f(i,j)+h(i,j)*zeta(i,j)
+        ENDIF
+        i = p
+        IF (z(p,j+1).LT.z(p,j-1)) THEN 
+          eta(i,j) = f(i,j)+h(i,j)*zeta(i,j)
+        ENDIF
+ 1111 CONTINUE
  
        DO 1200 j = 1, q-1
         DO 1210 i = 1, p-1
@@ -194,6 +203,9 @@ C      BEGIN THE ITERATIVE SOLUTION OF THE EQUATIONS
      3                        *(z(i+1,j)-z(i-1,j)) )
  1210   CONTINUE
  1200 CONTINUE
+CD      WRITE (*,9001) ((z(i,j),j=0,q),i=0,p)
+CD      WRITE (*,9002)
+CD      STOP
  
        DO 1300 j = 1, q-1
         DO 1310 i = 1, p-1
@@ -236,6 +248,9 @@ C      BEGIN THE ITERATIVE SOLUTION OF THE EQUATIONS
  1720     CONTINUE
  1710   CONTINUE
  1700 CONTINUE
+CD      WRITE (*,9001) ((z(i,j),j=0,q),i=0,p)
+CD      WRITE (*,9002)
+CD      STOP
 
 C      SECTION FOR CARRYING FORWARD THE EXTRAPOLATION 
        IF (k.EQ.0) THEN
@@ -253,6 +268,10 @@ C      SECTION FOR CARRYING FORWARD THE EXTRAPOLATION
  1830     CONTINUE
  1820   CONTINUE
       ENDIF
+      PRINT *,'k = ',k
+CD      WRITE (*,9001) ((z(i,j),j=0,q),i=0,p)
+CD      WRITE (*,9002)
+CD      STOP
 
 C      Apply the inflow/outflow conditions      
       DO 1900 j = 1, q-1
@@ -285,8 +304,8 @@ C      Apply the inflow/outflow conditions
         ENDIF
  1910 CONTINUE
        
-CD      WRITE (*,9001) ((z(i,j)-zi(i,j),j=0,q),i=0,p)
-CD      WRITE (*,9002)
+      WRITE (*,9001) ((z(i,j),j=0,q),i=0,p)
+      WRITE (*,9002)
       WRITE (1,9001) ((z(i,j),j=0,q),i=0,p)
       WRITE (1,9002)
  9001 FORMAT (16F6.1)
