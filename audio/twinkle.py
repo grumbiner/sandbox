05848be90d9702963f6d0372f2d26aa816eafc68
Robert Grumbine <grumbinespam@aol.com>
2023-01-16 15:49:49 -0500

    some clean up

--- audio/twinkle.py
+++ audio/twinkle.py
@@ -10,21 +10,21 @@ from music import *
 vol = 0.5
 
 n = []
-n.append( note(music.quarter_note, note.parse('C4'), vol) )
-n.append( note(music.quarter_note, note.parse('C4'), vol) )
-n.append( note(music.quarter_note, note.parse('G4'), vol) )
-n.append( note(music.quarter_note, note.parse('G4'), vol) )
-n.append( note(music.quarter_note, note.parse('A4'), vol) )
-n.append( note(music.quarter_note, note.parse('A4'), vol) )
-n.append( note(music.half_note, note.parse('G4'), vol) )
+n.append( note(music.quarter_note, note.parse('C5'), vol) )
+n.append( note(music.quarter_note, note.parse('C5'), vol) )
+n.append( note(music.quarter_note, note.parse('G5'), vol) )
+n.append( note(music.quarter_note, note.parse('G5'), vol) )
+n.append( note(music.quarter_note, note.parse('A5'), vol) )
+n.append( note(music.quarter_note, note.parse('A5'), vol) )
+n.append( note(music.half_note, note.parse('G5'), vol) )
 
-n.append( note(music.quarter_note, note.parse('F4'), vol) )
-n.append( note(music.quarter_note, note.parse('F4'), vol) )
-n.append( note(music.quarter_note, note.parse('E4'), vol) )
-n.append( note(music.quarter_note, note.parse('E4'), vol) )
-n.append( note(music.quarter_note, note.parse('D4'), vol) )
-n.append( note(music.quarter_note, note.parse('D4'), vol) )
-n.append( note(music.half_note, note.parse('C4'), vol) )
+n.append( note(music.quarter_note, note.parse('F5'), vol) )
+n.append( note(music.quarter_note, note.parse('F5'), vol) )
+n.append( note(music.quarter_note, note.parse('E5'), vol) )
+n.append( note(music.quarter_note, note.parse('E5'), vol) )
+n.append( note(music.quarter_note, note.parse('D5'), vol) )
+n.append( note(music.quarter_note, note.parse('D5'), vol) )
+n.append( note(music.half_note, note.parse('C5'), vol) )
 
 x = np.append(n[0].note,n[1].note)
 for i in range (2,len(n)):
